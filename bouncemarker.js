c7849d78bbfc645806d0a93296864cd2030b9046
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2025-08-28 15:31:48 -0700

    update leaflet to 2.0.0 alpha

--- bouncemarker.js
+++ bouncemarker.js
@@ -1,185 +1,302 @@
 /**
- * Copyright (C) 2013 Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
- * All Rights Reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * - Redistributions of source code must retain the above copyright notice,
- *   this list of conditions and the following disclaimer.
- *
- * - Redistributions in binary form must reproduce the above copyright notice,
- *   this list of conditions and the following disclaimer in the documentation
- *   and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
+ * @file Plugin for Leaflet.js to make a marker bounce when added to a map.
+ * @author Max Hadjinlian <max@m14n.dev>
+ * @copyright 2013-2025
+ * @license MIT
+ * @example <caption>All examples will assume a Leaflet map object</caption>
+ * import { Map, TileLayer } from 'leaflet';
+ * const map = new Map('map');
+ * new TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+ *   attribution: 'Map data &copy; <a href="http://www.osm.org">OpenStreetMap</a>'
+ * }).addTo(map);
  */
 
+/**
+ * User defined options
+ * @typedef {Object} bounceOnAddOptions
+ * @property {Number} [bounceOnAddOptions.duration=1000] - Animation's duration
+ * in ms.
+ * @property {Number} [bounceOnAddOptions.height=topY] - Height (in pixel) from
+ * which the marker is "dropped".
+ * @property {Number} [bounceOnAddOptions.loop=1] - Number of times the
+ * animation should play. -1 is a special value for infinite loop.
+ */
+
+// For Leaflet 2.0 compatibility, we need to extend the Marker class directly
+// This plugin will work when loaded after Leaflet is available on window.L
 (function() {
-  // Retain the value of the original onAdd and onRemove functions
-  let originalOnAdd = L.Marker.prototype.onAdd;
-  let originalOnRemove = L.Marker.prototype.onRemove;
+  /**
+   * Initialize the bounce marker plugin.
+   * Waits for Leaflet to be available on window.L
+   * and then extends the Marker class with bounce functionality.
+   * @private
+   * @return {void}
+   */
+  function initBounceMarker() {
+    // Check if Leaflet is available on window
+    if (!window.L) {
+      setTimeout(initBounceMarker, 100);
+      return;
+    }
 
-  // Add bounceonAdd options
-  L.Marker.mergeOptions({
-    bounceOnAdd: false,
-    bounceOnAddOptions: {
-      duration: 1000,
-      height: -1,
-      loop: 1,
-    },
-    bounceOnAddCallback: function() {},
-  });
+    const L = window.L;
 
-  L.Marker.include({
+    // Get the classes we need
+    const Marker = L.Marker;
+    const LatLng = L.LatLng;
+    const Point = L.Point;
 
-    _toPoint: function(latlng) {
-      return this._map.latLngToContainerPoint(latlng);
-    },
+    if (!Marker) {
+      console.error(
+          'Leaflet Marker class not found.',
+          'Make sure Leaflet is loaded before this plugin.',
+      );
+      return;
+    }
 
-    _toLatLng: function(point) {
-      return this._map.containerPointToLatLng(point);
-    },
+    // Retain the value of the original onAdd and onRemove functions
+    const originalOnAdd = Marker.prototype.onAdd;
+    const originalOnRemove = Marker.prototype.onRemove;
 
-    _motionStep: function(opts) {
-      let self = this;
-      let timePassed = new Date() - opts.start;
-      let progress = timePassed / opts.duration;
+    /**
+     * @namespace Marker
+     * @property {boolean} [bounceOnAdd=false] - bounce when added to the map.
+     * @property {bounceOnAddOptions} bounceOnAddOptions - user defined options
+     * @property {?bounceOnAddCallback} bounceOnAddCallback - run at the end of
+     * the animation
+     * @example
+     * new Marker([48.85, 2.35],
+     * {
+     *   bounceOnAdd: true,
+     *   bounceOnAddOptions: {duration: 500, height: 100, loop: 2},
+     *   bounceOnAddCallback: function() {console.log("done");}
+     * }).addTo(map);
+     */
 
-      if (progress > 1) {
-        progress = 1;
-      }
+    // Extend Marker options
+    Marker.mergeOptions({
+      bounceOnAdd: false,
+      bounceOnAddOptions: {
+        duration: 1000,
+        height: -1,
+        loop: 1,
+      },
+      /**
+       * Callback run at the end of the whole animation.
+       * @callback bounceOnAddCallback
+       * @return {void}
+       */
+      bounceOnAddCallback: function() {},
+    });
 
-      let delta = self._easeOutBounce(progress);
-      opts.step(delta);
+    // Extend Marker prototype
+    Object.assign(Marker.prototype, {
+      /**
+       * Helper to `latLngToContainerPoint` conversion method.
+       * @private
+       * @see {@link https://leafletjs.com/reference.html#map-latlngtocontainerpoint|Ref}
+       * @param {LatLng} latlng - geographical coordinate
+       * @return {Point} the corresponding pixel coordinate relative to the map
+       * container.
+       */
+      _toPoint: function(latlng) {
+        return this._map.latLngToContainerPoint(latlng);
+      },
 
-      if (progress === 1) {
-        opts.start = new Date();
-        progress = 0;
-        if (opts.loop > 0) opts.loop = opts.loop - 1;
-        if (opts.loop === 0) {
-          opts.end();
-          return;
+      /**
+       * Helper to `containerPointToLatLng` conversion method.
+       * @private
+       * @see {@link https://leafletjs.com/reference.html#map-containerPointToLatLng|Ref}
+       * @param {Point} point - pixel coordinate relative to the map container
+       * @return {LatLng} the corresponding geographical coordinate (for the
+       * current zoom level).
+       */
+      _toLatLng: function(point) {
+        return this._map.containerPointToLatLng(point);
+      },
+
+      /**
+       * Compute and update the marker's coordinate at every frame.
+       * @private
+       * @param {bounceOnAddOptions} opts - user defined options
+       */
+      _motionStep: function(opts) {
+        const self = this;
+        const timePassed = new Date() - opts.start;
+        let progress = timePassed / opts.duration;
+
+        if (progress > 1) {
+          progress = 1;
         }
-      }
 
-      self._animationId = L.Util.requestAnimFrame(function(timestamp) {
-        self._motionStep(opts);
-      });
-    },
+        const delta = self._easeOutBounce(progress);
+        opts.step(delta);
 
-    _bounceMotion: function(opts, callback) {
-      let original = L.latLng(this._origLatlng);
-      let start_y = this._dropPoint.y;
-      let start_x = this._dropPoint.x;
-      let distance = this._point.y - start_y;
-      let self = this;
+        if (progress === 1) {
+          opts.start = new Date();
+          progress = 0;
+          if (opts.loop > 0) opts.loop = opts.loop - 1;
+          if (opts.loop === 0) {
+            opts.end();
+            return;
+          }
+        }
 
-      self._animationId = L.Util.requestAnimFrame(function() {
-        self._motionStep({
-          duration: opts.duration || 1000, // 1 sec by default
-          loop: opts.loop || 1,
-          start: new Date(),
-          step: function(delta) {
-            self._dropPoint.y =
-              start_y
-            + (distance * delta)
-            - (self._map.project(self._map.getCenter()).y - self._origMapCenter.y);
-            self._dropPoint.x =
-              start_x
-            - (self._map.project(self._map.getCenter()).x - self._origMapCenter.x);
-            self.setLatLng(self._toLatLng(self._dropPoint));
-          },
-          end: function() {
-            self.setLatLng(original);
-            if (typeof callback === 'function') callback();
-          },
+        self._animationId = requestAnimationFrame(function(timestamp) {
+          self._motionStep(opts);
         });
-      });
-    },
+      },
 
-    // Many thanks to Robert Penner for this function
-    _easeOutBounce: function(pos) {
-      if ((pos) < (1 / 2.75)) {
-        return (7.5625 * pos * pos);
-      } else if (pos < (2 / 2.75)) {
-        return (7.5625 * (pos -= (1.5 / 2.75)) * pos + 0.75);
-      } else if (pos < (2.5 / 2.75)) {
-        return (7.5625 * (pos -= (2.25 / 2.75)) * pos + 0.9375);
-      } else {
-        return (7.5625 * (pos -= (2.625 / 2.75)) * pos + 0.984375);
-      }
-    },
+      /**
+       * Wrapper around _motionStep; takes care of computing coordinates and
+       * calling callback at the end.
+       * @see {@link _motionStep}
+       * @private
+       * @param {bounceOnAddOptions} opts - user defined options
+       * @param {?bounceOnAddCallback} callback - user defined callback
+       */
+      _bounceMotion: function(opts, callback) {
+        const original = new LatLng(this._origLatlng.lat, this._origLatlng.lng);
+        const startY = this._dropPoint.y;
+        const startX = this._dropPoint.x;
+        const distance = this._point.y - startY;
+        const self = this;
+        const map = self._map;
 
-    // Bounce : if options.height in pixels is not specified, drop from top.
-    // If options.duration is not specified animation is 1s long.
-    bounce: function(options, endCallback) {
-      if (typeof options === 'function') {
-        endCallback = options;
-        options = null;
-      }
-      options = options || {duration: 1000, height: -1, loop: 1};
+        self._animationId = requestAnimationFrame(function() {
+          self._motionStep({
+            duration: opts.duration || 1000, // 1 sec by default
+            loop: opts.loop || 1,
+            start: new Date(),
+            step: function(delta) {
+              self._dropPoint.y =
+                startY +
+                distance * delta -
+                (map.project(map.getCenter()).y - self._origMapCenter.y);
+              self._dropPoint.x =
+                startX -
+                (map.project(map.getCenter()).x - self._origMapCenter.x);
+              self.setLatLng(self._toLatLng(self._dropPoint));
+            },
+            end: function() {
+              self.setLatLng(original);
+              if (typeof callback === 'function') callback();
+            },
+          });
+        });
+      },
 
-      // backward compatibility
-      if (typeof options === 'number') {
-        options.duration = arguments[0];
-        options.height = arguments[1];
-      }
+      /**
+       * Rate of change of progress over time using the easeOut function.
+       * Many thanks to Robert Penner for this function
+       * @see {@link https://easings.net/#easeOutBounce}
+       * @private
+       * @param {float } progress - current progress on the curve
+       * @return {float} Next progress value
+       */
+      _easeOutBounce: function(progress) {
+        if (progress < 1 / 2.75) {
+          return 7.5625 * progress * progress;
+        } else if (progress < 2 / 2.75) {
+          return 7.5625 * (progress -= 1.5 / 2.75) * progress + 0.75;
+        } else if (progress < 2.5 / 2.75) {
+          return 7.5625 * (progress -= 2.25 / 2.75) * progress + 0.9375;
+        } else {
+          return 7.5625 * (progress -= 2.625 / 2.75) * progress + 0.984375;
+        }
+      },
 
-      // Keep original map center
-      this._origMapCenter = this._map.project(this._map.getCenter());
-      this._dropPoint = this._getDropPoint(options.height);
-      this._bounceMotion(options, endCallback);
-    },
+      /**
+       * Helper to get a drop point from a height.
+       * @private
+       * @param {Number} [height=topY]
+       * @return {Point} Current new Point instance with new coordinates
+       */
+      _getDropPoint: function(height) {
+        // Get current coordidates in pixel
+        this._point = this._toPoint(this._origLatlng);
+        let topY;
+        if (height === undefined || height < 0) {
+          topY = this._toPoint(this._map.getBounds()._northEast).y;
+        } else {
+          topY = this._point.y - height;
+        }
+        return new Point(this._point.x, topY);
+      },
 
-    stopBounce: function(){
-      // We may have modified the marker; so we need to place it where it
-      // belongs so next time its coordinates are not changed.
-      this.setLatLng(this._origLatlng);
-      L.Util.cancelAnimFrame(this._animationId);
-    },
+      /**
+       * Make a marker bounce at anytime you wish.
+       * @param {bounceOnAddOptions} options - user defined options
+       * @param {?bounceOnAddCallback} endCallback - run at end of animation
+       * @example
+       * marker = new Marker([48.85, 2.35], {bounceOnAdd: true}).addTo(map);
+       * marker.on('click', function() {
+       *     marker.bounce({duration: 500, height: 100});
+       * });
+       */
+      bounce: function(options, endCallback) {
+        if (typeof options === 'function') {
+          endCallback = options;
+          options = null;
+        }
+        options = options || {duration: 1000, height: -1, loop: 1};
 
-    // This will get you a drop point given a height.
-    // If no height is given, the top y will be used.
-    _getDropPoint: function(height) {
-      // Get current coordidates in pixel
-      this._point = this._toPoint(this._origLatlng);
-      let top_y;
-      if (height === undefined || height < 0) {
-        top_y = this._toPoint(this._map.getBounds()._northEast).y;
-      } else {
-        top_y = this._point.y - height;
-      }
-      return new L.Point(this._point.x, top_y);
-    },
+        // Keep original latitude, longitude and map center
+        this._origLatlng = this.getLatLng();
+        this._origMapCenter = this._map.project(this._map.getCenter());
+        this._dropPoint = this._getDropPoint(options.height);
+        this._bounceMotion(options, endCallback);
+      },
 
-    onAdd: function(map) {
-      this._map = map;
+      /**
+       * Stop the animation and place the marker at its destination.
+       * @return {void}
+       */
+      stopBounce: function() {
+        // We may have modified the marker; so we need to place it where it
+        // belongs so next time its coordinates are not changed.
+        if (typeof this._origLatlng !== 'undefined') {
+          this.setLatLng(this._origLatlng);
+        }
+        cancelAnimationFrame(this._animationId);
+      },
 
-      // Call leaflet original method to add the Marker to the map.
-      originalOnAdd.call(this, map);
+      /**
+       * Add a Marker to {map} and optionaly make it bounce.
+       * @override
+       * @param {Map} map - Leaflet map to add the marker to
+       * @example
+       * new Marker([48.85, 2.35],
+       *   {
+       *     bounceOnAdd: true,
+       *   }).addTo(map);
+       */
+      onAdd: function(map) {
+        this._map = map;
 
-      // Keep original latitude and longitude
-      this._origLatlng = this.getLatLng();
+        // Call leaflet original method to add the Marker to the map.
+        originalOnAdd.call(this, map);
 
-      if (this.options.bounceOnAdd === true) {
-        this.bounce(this.options.bounceOnAddOptions, this.options.bounceOnAddCallback);
-      }
-    },
+        if (this.options.bounceOnAdd === true) {
+          this.bounce(
+              this.options.bounceOnAddOptions,
+              this.options.bounceOnAddCallback,
+          );
+        }
+      },
 
-    onRemove: function(map) {
-      this.stopBounce();
-      originalOnRemove.call(this, map);
-    },
-  });
+      /**
+       * Stop any animation running and remove the Marker from {map}.
+       * @override
+       * @param {Map} map - Leaflet map to add the marker to
+       */
+      onRemove: function(map) {
+        this.stopBounce();
+        originalOnRemove.call(this, map);
+      },
+    });
+  }
+
+  // Initialize the plugin
+  initBounceMarker();
 })();
